79ac90738324cf5cb8a61d5f3839f16f9e1049a9
Nazar-svg <nazar.kolos90@gmail.com>
2020-08-19 16:13:38 +0300

    Iplement navigation in my project

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 import classes from './Navigation.module.css'
+import { NavLink } from 'react-router-dom'
+
+const links = [
+    { to: '/cycling', label: 'Вело Треніровки', exact: false },
+    { to: '/travel', label: 'Подорожі', exact: false }
+]
 
 class Navigation extends Component {
+
+    renderLinks() {
+        return links.map((link, index) => {
+            return (
+                <NavLink
+                    className={classes.navLink}
+                    to={link.to}
+                    key={index}
+                    exact={link.exact}
+                    activeClassName={classes.active}
+                    >
+                    {link.label}
+                </NavLink>
+            )
+        })
+    }
     render() {
         return (
-               <nav className={classes.headerNav}>
-                        <a className={classes.navLink}>cycling training</a>
-                        <a className={classes.navLink}>travel</a>
-                        <a className={classes.navLink}>POWER TRAINING</a>
-                        <a className={classes.navLink}>RUN training</a>
-                        <a className={classes.navLink}>Photo</a>
-                    </nav>
+            <>
+                <nav className={classes.headerNav}>
+                    <ul>
+                        <li>
+                            {this.renderLinks()}
+                        </li>
+                    </ul>
+                </nav>
+            </>
         )
     }
 
